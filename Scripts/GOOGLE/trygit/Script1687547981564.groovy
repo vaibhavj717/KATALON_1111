e17bd056f377ee830d22f15493dfebd8a3dac2da
vaibhavj717 <126074042+vaibhavj717@users.noreply.github.com>
2023-07-08 13:09:13 +0530

    Add files via upload

---
+++ Scripts/GOOGLE/trygit/Script1687547981564.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.configuration.RunConfiguration as RC
+
+//RC.setExecutionSourceId('97f30753dbee3edda39cc6ef7c0af06dc109e6a1')  // Replace with your execution source ID
+//RC.setRemoteWebDriverUrl(' https://gauravsavla_3qbydV:v4MZJdWuLsKQSyezExcV@hub-cloud.browserstack.com/wd/hub')    // Replace with your BrowserStack URL
+//RC.setDriverPreferences(['browserstack.user': 'gauravsavla_3qbydV', // Replace with your BrowserStack username
+//						 'browserstack.key': 'v4MZJdWuLsKQSyezExcV', // Replace with your BrowserStack access key
+//						 'executionSource': 'internal'])
+//RC.setRemoteWebDriverUrl(' https://gauravsavla_3qbydV:v4MZJdWuLsKQSyezExcV@hub-cloud.browserstack.com/wd/hub')  // Replace with your BrowserStack URL
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.google.com/')
+
+WebUI.takeScreenshotAsCheckpoint('Webpage')
+
+WebUI.closeBrowser()
+
